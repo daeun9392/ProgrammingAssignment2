1687093f1455e356a5c563f3385fcb4e8ff68970
daeun9392 <98995767+daeun9392@users.noreply.github.com>
2022-02-04 11:52:27 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,36 @@
 ## functions do
 
 ## Write a short comment describing this function
+## makeCacheMatrix consists of set,get,setinv,getinv
+##library(MASS) is used to calculate inverse for non squared as well as sqare matrices
 
-makeCacheMatrix <- function(x = matrix()) {
-
+library(MASS)
+makeCacheMatrix <- function(x = matrix()) { ## define the argument with default mode of "matrix"
+    inv <- NULL                             ## initialize inv as NULL; will hold value of matrix inverse 
+    set <- function(y) {                    ## define the set function to assign new 
+        x <<- y                             ## value of matrix in parent environment
+        inv <<- NULL                        ## if there is a new matrix, reset inv to NULL
+    }
+    get <- function() x                     ## define the get fucntion - returns value of the matrix argument
+    
+    setinverse <- function(inverse) inv <<- inverse  ## assigns value of inv in parent environment
+    getinverse <- function() inv                     ## gets the value of inv where called
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  ## you need this in order to refer 
+    ## to the functions with the $ operator
 }
 
-
 ## Write a short comment describing this function
+## This is used to get the cache data
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)){
+        message("getting cached data!")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data,...)
+    x$setinv(inv)
+    inv
 }
